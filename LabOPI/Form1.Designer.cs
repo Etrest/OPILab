8add64827b597d19dc9d4f10ec2d58b85b593c63
Etrest <89696691+Etrest@users.noreply.github.com>
2022-01-23 15:25:16 +0300

    Add files via upload
    ver 1.0

---
+++ LabOPI/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace LabOPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.wrsDataSet = new LabOPI.wrsDataSet();
+            this.empBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.empTableAdapter = new LabOPI.wrsDataSetTableAdapters.empTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.Перевод = new System.Windows.Forms.Button();
+            this.empidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empocDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empprofDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wrsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.empidDataGridViewTextBoxColumn,
+            this.empnameDataGridViewTextBoxColumn,
+            this.empocDataGridViewTextBoxColumn,
+            this.empprofDataGridViewTextBoxColumn,
+            this.empdDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.empBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(649, 214);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // wrsDataSet
+            // 
+            this.wrsDataSet.DataSetName = "wrsDataSet";
+            this.wrsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // empBindingSource
+            // 
+            this.empBindingSource.DataMember = "emp";
+            this.empBindingSource.DataSource = this.wrsDataSet;
+            // 
+            // empTableAdapter
+            // 
+            this.empTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Увольнение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Добавление";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(51, 310);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(206, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(51, 232);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(51, 258);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(206, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(51, 284);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(206, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(475, 232);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(206, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(475, 258);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(206, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // Перевод
+            // 
+            this.Перевод.Location = new System.Drawing.Point(698, 229);
+            this.Перевод.Name = "Перевод";
+            this.Перевод.Size = new System.Drawing.Size(75, 23);
+            this.Перевод.TabIndex = 10;
+            this.Перевод.Text = "Переназначить";
+            this.Перевод.UseVisualStyleBackColor = true;
+            this.Перевод.Click += new System.EventHandler(this.Перевод_Click);
+            // 
+            // empidDataGridViewTextBoxColumn
+            // 
+            this.empidDataGridViewTextBoxColumn.DataPropertyName = "emp_id";
+            this.empidDataGridViewTextBoxColumn.HeaderText = "Табель Сотрудника";
+            this.empidDataGridViewTextBoxColumn.Name = "empidDataGridViewTextBoxColumn";
+            this.empidDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // empnameDataGridViewTextBoxColumn
+            // 
+            this.empnameDataGridViewTextBoxColumn.DataPropertyName = "emp_name";
+            this.empnameDataGridViewTextBoxColumn.HeaderText = "emp_name";
+            this.empnameDataGridViewTextBoxColumn.Name = "empnameDataGridViewTextBoxColumn";
+            // 
+            // empocDataGridViewTextBoxColumn
+            // 
+            this.empocDataGridViewTextBoxColumn.DataPropertyName = "emp_oc";
+            this.empocDataGridViewTextBoxColumn.HeaderText = "emp_oc";
+            this.empocDataGridViewTextBoxColumn.Name = "empocDataGridViewTextBoxColumn";
+            // 
+            // empprofDataGridViewTextBoxColumn
+            // 
+            this.empprofDataGridViewTextBoxColumn.DataPropertyName = "emp_prof";
+            this.empprofDataGridViewTextBoxColumn.HeaderText = "emp_prof";
+            this.empprofDataGridViewTextBoxColumn.Name = "empprofDataGridViewTextBoxColumn";
+            // 
+            // empdDataGridViewTextBoxColumn
+            // 
+            this.empdDataGridViewTextBoxColumn.DataPropertyName = "emp_d";
+            this.empdDataGridViewTextBoxColumn.HeaderText = "emp_d";
+            this.empdDataGridViewTextBoxColumn.Name = "empdDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Перевод);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wrsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private wrsDataSet wrsDataSet;
+        private System.Windows.Forms.BindingSource empBindingSource;
+        private wrsDataSetTableAdapters.empTableAdapter empTableAdapter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button Перевод;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empocDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empprofDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empdDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ LabOPI/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabOPI
+{
+    public partial class Form1 : Form
+    {
+        public static string connectString = "Provider = Microsoft.Jet.OLEDB.4.0;Data Source = wrs.mdb";
+        private OleDbConnection myConnection;
+        public Form1()
+        {
+            InitializeComponent();
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "wrsDataSet.emp". При необходимости она может быть перемещена или удалена.
+            this.empTableAdapter.Fill(this.wrsDataSet.emp);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int emp_id = Convert.ToInt32(textBox1.Text);
+            string query = "DELETE FROM emp WHERE [emp_id] = " + emp_id;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Данный работник уволен");
+            this.empTableAdapter.Fill(this.wrsDataSet.emp);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            string Name2 = textBox2.Text;
+            string Name3 = textBox3.Text;
+            string Name4 = textBox4.Text;
+            string Position3 = dateTimePicker1.Value.ToString("dd-MM-yyyy");
+            string query = "INSERT INTO emp (emp_name, emp_oc, emp_prof, emp_d) VALUES ('" + Name2 + "','" + Name3 + "','" + Name4 + "','" + Position3 + "')";
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Работник добавлен");
+            this.empTableAdapter.Fill(this.wrsDataSet.emp);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Перевод_Click(object sender, EventArgs e)
+        {
+            string name2 = Convert.ToString(textBox5.Text);
+
+            string query = "UPDATE emp SET emp_oc ='" + textBox6.Text + "' WHERE [emp_id] = " + name2;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Изменение добавлены");
+            this.empTableAdapter.Fill(this.wrsDataSet.emp);
+        }
+    }
+}
